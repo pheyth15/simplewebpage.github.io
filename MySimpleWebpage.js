721f4b36d5d87c164ecdd3f727e168c8ee5a119f
Kevin <53475718+pheyth15@users.noreply.github.com>
2020-09-25 22:16:51 +0800

    publish

---
+++ MySimpleWebpage.js
@@ -0,0 +1,9 @@
+//Alert Button for "About Me"
+function myFunction(){
+    alert("Hello! My name is Kevin Daniel Almirante"+"\n"+"Age: 17"+"\n"+"Birthdate: October 26, 2002"+"\n"+"I'm currently a grade 12 student at STI Bacoor");
+}
+
+//Simple Sentence used to stylized at CSS
+document.write("<p class ='waw1'>\"The Difficulties I've encountered were having a slow and not consistent </p>")
+document.write("<p class ='waw'>internet that keeps me disconnecting during my classes. That is </p>")
+document.write("<p class ='waw'>why I have my backup sim so that I wont miss my classes.\"</p>")
